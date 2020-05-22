f3d1d5aa584215ae2b0896a04500bb43aa603995
RAYDAN GASPAR <raydangaspar@gmail.com>
2020-05-22 15:00:28 -0300

    Date validation

--- passagens/validation.py
+++ passagens/validation.py
@@ -7,3 +7,13 @@ def campo_tem_algum_numero(valor_campo, nome_campo, lista_de_erros):
     """Verifica se possui algum dígito numérico"""
     if any(char.isdigit() for char in valor_campo):
         lista_de_erros[nome_campo] = 'Não inclua números neste campo'
+
+def data_ida_maior_que_data_volta(data_ida, data_volta, lista_de_erros):
+    """verifica se data de ida é maior que data de volta"""
+    if data_ida > data_volta:
+        lista_de_erros['data_volta'] = 'Data de volta não pode ser menor que data de ida'
+
+def data_ida_menor_que_data_de_hoje(data_ida, data_pesquisa, lista_de_erros):
+    """Verifica se data de ida é menor que data de hoje"""
+    if data_ida < data_pesquisa:
+        lista_de_erros['data_ida'] = 'Data de ida não pode ser menor que hoje'
