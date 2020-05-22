d8b30c6831bf33efba70551d4555834969bb8143
RAYDAN GASPAR <raydangaspar@gmail.com>
2020-05-22 14:47:38 -0300

    Clean

--- passagens/forms.py
+++ passagens/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from tempus_dominus.widgets import DatePicker
 from datetime import datetime
 from passagens.classe_viagem import tipos_de_classe
+from passagens.validation import *
 
 class PassagemForms(forms.Form):
     origem = forms.CharField(label= 'Origem', max_length=100)
@@ -18,16 +19,15 @@ class PassagemForms(forms.Form):
     )
     email = forms.EmailField(label='Email', max_length=150)
 
-    def clean_origem(self):
+    def clean(self):
         origem = self.cleaned_data.get('origem')
-        if any(char.isdigit() for char in origem):
-            raise forms.ValidationError('Origem inválida: Não inclua números')
-        else:
-            return origem
-
-    def clean_destino(self):
         destino = self.cleaned_data.get('destino')
-        if any(char.isdigit() for char in destino):
-            raise forms.ValidationError('Destino inválido: Não inclua números')
-        else:
-            return destino
+        lista_de_erros = {}
+        campo_tem_algum_numero(origem, 'origem', lista_de_erros)
+        campo_tem_algum_numero(destino, 'destino', lista_de_erros)
+        origem_destino_iguais(origem, destino, lista_de_erros)
+        if lista_de_erros is not None:
+            for erro in lista_de_erros:
+                mensagem_erro = lista_de_erros[erro]
+                self.add_error(erro, mensagem_erro)
+        return self.cleaned_data
